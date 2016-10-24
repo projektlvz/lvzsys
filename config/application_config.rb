1c294b0ff19708c9bc9fa09a68512db40ff2f7f4
thomasba86 <thomas@thomas-bayer.de>
2016-10-24 13:03:40 +0300

    Initial commit of the project + working CE.

---
+++ config/application_config.rb
@@ -0,0 +1,5 @@
+configatron.community_name = "Green Store"
+configatron.community_description = "Description here"
+configatron.support_email =  "test@test.com"
+configatron.meta_description =  'A description of your community for use in META tags.'
+configatron.meta_keywords =  'keywords for use in META tags'
