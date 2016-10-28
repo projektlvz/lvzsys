4069b9b1e3b067d7df3475643dbdfb4fe4927d57
thomasba86 <thomas@thomas-bayer.de>
2016-10-28 19:08:02 +0300

    Working map filtering.

--- app/assets/javascripts/custom_map.js
+++ app/assets/javascripts/custom_map.js
@@ -1,8 +1,9 @@
+var markers = null;
+
 window.onload = function() {
     handler = Gmaps.build('Google');
     handler.buildMap({provider: {}, internal: {id: 'map'}}, function () {
         handler.map.centerOn({ lat: 52.5125394, lng: 13.3421765 })
-        var markers = null;
         $.get( "/get_shops", function() {})
             .done(function(shops_json) {
                 markers = handler.addMarkers(shops_json);
@@ -10,7 +11,32 @@ window.onload = function() {
                 handler.fitMapToBounds();
             })
             .fail(function() {
-                alert( "error" );
+                console.log( "Error retrieving basic shops list." );
             });
     });
+};
+
+function GetFormData(){
+    var data = {};
+    data.shop_category = $( "#s_shop_category").val();
+    data.features = [];
+    var checkbox_data = $("input[id*='s_']:checked");
+    checkbox_data.each(function(x){
+        data.features.push($(checkbox_data[x]).attr("id").substring(2))
+    });
+    data.location = $( "#s_city").val();
+    return {search_params: data};
+};
+
+function RetrieveShops(){
+    $.get( "/get_shops", GetFormData())
+        .done(function(shops_json) {
+            handler.removeMarkers(markers)
+            markers = handler.addMarkers(shops_json);
+            handler.bounds.extendWith(markers);
+            handler.fitMapToBounds();
+        })
+        .fail(function() {
+            console.log( "Error retrieving filtered shops list." );
+        });
 };
--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,11 +1,35 @@
 
 class MapController < ApplicationController
   def get_shops
-    @hash = Gmaps4rails.build_markers(Shop.all) do |shop, marker|
-      marker.lat shop.latitude
-      marker.lng shop.longitude
+    @hash = nil
+    if params[:search_params].present?
+      search_params = params[:search_params]
+
+      shops = Shop.where('latitude is not null and longitude is not null')
+      shops = shops.where(category: search_params['shop_category']) if search_params['shop_category'].present?
+
+      if search_params['features'].present?
+        search_params['features'].each do |feature_name|
+          shops = shops.where("features @> hstore(:key, :value)",     key: feature_name, value: "1")
+        end
+      end
+
+      shops = shops.near(search_params['location'], ENV['GOOGLE_MAP_SEARCH_RADIUS'] || 50) if search_params['location'].present?
+
+      @hash = shops_to_coords(shops.all)
+    else
+      @hash = shops_to_coords(Shop.all)
     end
 
     render json: @hash.to_json
+  end
+
+  private
+
+  def shops_to_coords(shop)
+    Gmaps4rails.build_markers(shop) do |shop, marker|
+      marker.lat shop.latitude
+      marker.lng shop.longitude
+    end
   end
 end
