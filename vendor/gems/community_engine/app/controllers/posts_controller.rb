752c8cce758a51060ae9ab392997004e7cc7f57c
thomasba86 <thomas@thomas-bayer.de>
2016-11-12 15:34:47 +0300

    Fix for popular and recent.

--- vendor/gems/community_engine/app/controllers/posts_controller.rb
+++ vendor/gems/community_engine/app/controllers/posts_controller.rb
@@ -199,9 +199,9 @@ class PostsController < BaseController
 
 
   def popular
-    @posts = Post.find_popular({:limit => 15, :since => 3.days}).where('owner_post = FALSE').where(user_id: current_user.friendships.map{ |f| f.friend_id})
+    @posts = Post.find_popular({:limit => 15, :since => 20.days}).where(user_id: current_user.friendships.map{ |f| f.friend_id})
 
-    @monthly_popular_posts = Post.find_popular({:limit => 20, :since => 30.days}).where('owner_post = FALSE').where(user_id: current_user.friendships.map{ |f| f.friend_id})
+    @monthly_popular_posts = Post.find_popular({:limit => 20, :since => 30.days}).where(user_id: current_user.friendships.map{ |f| f.friend_id})
 
     @related_tags = ActsAsTaggableOn::Tag.find_by_sql("SELECT tags.id, tags.name, count(*) AS count
       FROM taggings, tags
@@ -220,7 +220,7 @@ class PostsController < BaseController
   end
 
   def recent
-    @posts = Post.where('owner_post = TRUE').where(user_id: current_user.friendships.map{ |f| f.friend_id}).recent.page(params[:page]).per(20)
+    @posts = Post.where(user_id: current_user.friendships.map{ |f| f.friend_id}).recent.page(params[:page]).per(20)
 
     @recent_clippings = Clipping.find_recent(:limit => 15)
     @recent_photos = Photo.find_recent(:limit => 10)
