df8ccb9c3038d26f27b2852bfc5e6776f7a1b6b9
thomasba86 <thomas@thomas-bayer.de>
2016-11-12 15:22:39 +0300

    Animation and tooltips for likes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
 //= require underscore
 //= require gmaps/google
 //= require jquery_nested_form
--- vendor/gems/community_engine/app/assets/javascripts/score_setter.js
+++ vendor/gems/community_engine/app/assets/javascripts/score_setter.js
@@ -1,7 +1,9 @@
 function setScore(score, post_id, giver_id){
     $.post( "/post/score",{ score: score, post_id: post_id, giver_id: giver_id }, function() {
-        alert( "success" );
+        $('#'+score).height(50).width(50);
+        window.setTimeout(function() {$('#'+score).height(34).width(34);}, 1000);
+
     }).fail(function() {
-        alert( "error" );
+
     });
 }
