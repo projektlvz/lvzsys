a28a7de505e8439662d4022572bd12aa3c9cc020
thomasba86 <thomas@thomas-bayer.de>
2016-11-28 17:49:44 +0300

    Minor style fixes. Proper search for categories based on slug, not name.

--- app/assets/javascripts/custom_map.js
+++ app/assets/javascripts/custom_map.js
@@ -7,7 +7,7 @@ window.onload = function() {
     
     handler = Gmaps.build('Google');
     handler.buildMap({provider: {}, internal: {id: 'map'}}, function () {
-        handler.map.centerOn({ lat: 52.5125394, lng: 13.3421765 })
+        handler.map.centerOn({ lat: 52.5125394, lng: 13.3421765 });
         $.get( "/get_shops", function() {})
             .done(function(shops_json) {
                 markers = handler.addMarkers(shops_json);
@@ -28,18 +28,18 @@ function GetFormData(){
     checkbox_data.each(function(x){
         data.features.push($(checkbox_data[x]).attr("id").substring(2))
     });
-    data.location = $( "#s_city").val();
+    data.location = $("#s_city").val();
     return {search_params: data};
-};
+}
 
 function RetrieveShops(){
     $.get( "/get_shops", GetFormData())
         .done(function(shops_json) {
-            handler.removeMarkers(markers)
+            handler.removeMarkers(markers);
             markers = handler.addMarkers(shops_json);
             handler.bounds.extendWith(markers);
         })
         .fail(function() {
             console.log( "Error retrieving filtered shops list." );
         });
-};
+}
