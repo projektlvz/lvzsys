f4bb12784dc35bff3b16689928be316673c80a3c
projektlvz <ththth@hotmail.de>
2016-12-04 19:28:00 +0100

    Add files via upload

--- config/application_config.rb
+++ config/application_config.rb
@@ -1,4 +1,4 @@
-configatron.community_name = "Green Store"
+configatron.community_name = "Lokal VZ"
 configatron.community_description = "Description here"
 configatron.support_email =  "test@test.com"
 configatron.meta_description =  'A description of your community for use in META tags.'
