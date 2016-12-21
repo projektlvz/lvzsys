9ad7195a600db4dc1df01ff7100c306a95d388ae
projektlvz <ththth@hotmail.de>
2016-12-21 11:35:08 +0100

    Add files via upload

--- config/application_config.rb
+++ config/application_config.rb
@@ -1,5 +1,5 @@
 configatron.community_name = "Lokal VZ"
 configatron.community_description = "Description here"
-configatron.support_email =  "test@test.com"
+configatron.support_email =  "thomas@thomas-bayer.de"
 configatron.meta_description =  'A description of your community for use in META tags.'
 configatron.meta_keywords =  'keywords for use in META tags'
