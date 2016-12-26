c9f401759518003ac535765942fdc56d8d1df8af
projektlvz <ththth@hotmail.de>
2016-12-26 22:17:13 +0100

    Delete point.rb

--- vendor/gems/community_engine/app/models/point.rb
+++
@@ -1,6 +0,0 @@
-class Point < ActiveRecord::Base
-  validates_presence_of :owner_id, :owner_type, :giver_id, :giver_type, :score, :operation
-
-  belongs_to :owner, polymorphic: true
-  belongs_to :giver, polymorphic: true
-end
